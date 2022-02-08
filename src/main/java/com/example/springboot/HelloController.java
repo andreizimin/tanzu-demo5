a9c8cf18a2c91a29e0817e7a4f5e9a40ead67554
andreizimin <75641133+andreizimin@users.noreply.github.com>
2022-02-08 10:09:28 -0800

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! Revision 1";
+		return "Greetings from Spring Boot + Tanzu! Branch andreizimin";
 	}
 
 }
