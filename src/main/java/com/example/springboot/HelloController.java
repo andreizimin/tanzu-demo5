68fd4ecaf5e6357c4872049cad07d524cf213f0f
andreizimin <andrei.zimin@gmail.com>
2022-02-11 02:14:40 -0800

    revision 0

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! Revision 2";
+		return "Finance Application, Microservice 1, Branch main, Revision 0";
 	}
 
 }
