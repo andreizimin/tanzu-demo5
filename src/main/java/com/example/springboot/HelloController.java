9b9aaeae181a342dadd4c9ca9273fff1a408ec7e
andreizimin <andrei.zimin@gmail.com>
2022-02-11 09:00:10 -0800

    commit

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! Branch andreizimin, Revision 2";
+		return "Microservice belonging to ComponentZ";
 	}
 
 }
